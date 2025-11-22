9fd461f7e0a8bc3767883c320c9ffa9dc5c9c6ec
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-11-22 17:07:47 +0500

    NodeJS-Log-Arguments

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const getArgs = require("./helpers/args");
 const startCLI = () => {
   const args = getArgs(process.argv);
   console.log(args);
+
+  if (args.h) {
+    // Help
+  }
+  if (args.s) {
+    // save city
+  }
+  if (args.t) {
+    // save token
+  }
+  //result
 };
 
 startCLI();
