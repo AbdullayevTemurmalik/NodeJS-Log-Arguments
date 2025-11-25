6d2ac07bf4d8b75bf7364ea078fee49536244978
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-11-25 19:03:44 +0500

    Node-js-Weather-CLI

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import getArgs from "./helpers/args.js";
 import { printError, printSuccess, printHelp } from "./services/log.service.js";
 import { saveKeyValue } from "./services/storage.service.js";
 
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token saved successfully");
+  } catch (error) {
+    printError(error.message);
+  }
+};
+
 const startCLI = () => {
   const args = getArgs(process.argv);
   console.log(args);
@@ -14,7 +23,7 @@ const startCLI = () => {
     // save city
   }
   if (args.t) {
-    saveKeyValue("token", args.t);
+    return saveToken(args.t);
     // save token
   }
   //result
--- services/storage.service.js
+++ services/storage.service.js
@@ -1,8 +1,37 @@
 import os from "os";
 import path from "path";
+import fs from "fs";
 
-const saveKeyValue = (key, value) => {
-  console.log(path.join(os.homedir(), "weather-dataq.json"));
+const filePath = path.join(os.homedir(), "weather-data.json");
+
+const isExist = async (path) => {
+  try {
+    await fs.promises.stat(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 
-export { saveKeyValue };
+const saveKeyValue = async (key, value) => {
+  let data = {}; // <-- const emas, let !!!
+
+  if (await isExist(filePath)) {
+    const file = await fs.promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+
+  data[key] = value;
+
+  await fs.promises.writeFile(filePath, JSON.stringify(data));
+};
+
+const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    const file = await fs.promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+};
+
+export { saveKeyValue, getKeyValue };
