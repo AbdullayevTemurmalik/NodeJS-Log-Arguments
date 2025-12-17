ce59ba53af12c1acd5eb53b5eb4c800f3cdf68b2
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-12-17 14:18:40 +0500

    Node-js-Weather-CLI

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 import getArgs from "./helpers/args.js";
-import { printError, printSuccess, printHelp } from "./services/log.service.js";
+import { printHelp, printError, printSuccess } from "./services/log.service.js";
 import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
-import { getWeather } from "./services/weather.service.js";
+import { getWeather } from "./services/api.service.js";
 
 const saveToken = async (token) => {
-  if (!token) {
+  if (!token.length) {
     printError("Token not provided");
     return;
   }
+
   try {
     await saveKeyValue(TOKEN_DICTIONARY.token, token);
     printSuccess("Token saved successfully");
-  } catch (error) {
-    printError(error.message);
+  } catch (err) {
+    printError(err.message);
+  }
+};
+
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError("City not provided");
+    return;
+  }
+
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess("City saved successfully");
+  } catch (err) {
+    printError(err.message);
   }
 };
 
 const startCLI = () => {
   const args = getArgs(process.argv);
 
-  if (args.h) printHelp();
+  if (args.h) return printHelp();
+  if (args.t) return saveToken(args.t);
+  if (args.s) return saveCity(args.s);
 
-  if (args.s) {
-    // save city
-  }
-
-  if (args.t) {
-    return saveToken(args.t);
-  }
-
-  getWeather("uzbekistan");
+  getWeather();
 };
 
 startCLI();
--- services/api.service.js
+++ services/api.service.js
@@ -7,13 +7,15 @@ const getWeather = async (city) => {
     throw new Error("API doen't exist, -t [API_KEY] for saving token");
   }
 
-  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+  const url = new URL(
+    "https://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=83c3b71ca0e9968c63e6e736446e26a6"
+  );
   url.searchParams.append("q", city);
-  url.searchParams.append("appid");
+  url.searchParams.append("appid", token);
   url.searchParams.append("lang", "en");
   url.searchParams.append("units", "metric");
 
-  https.get(url, () => {
+  https.get(url, (response) => {
     let res = "";
     response.on("data", (chunk) => {
       res += chunk;
